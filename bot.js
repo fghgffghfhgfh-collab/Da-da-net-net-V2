ee748365ee5c1d456e394fc66d670c26468bae05
fghgffghfhgfh-collab <fghgffghfhgfh@gmail.com>
2025-11-30 00:28:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,1 @@
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf('8242775699:AAGkMtfFgroB8MqJ3trpIZ9CHpuHguuVSAg'); // токен, который вы получили от BotFather
 
-bot.start((ctx) => {
-  ctx.reply('Открыть миниаппу', {
-    reply_markup: {
-      keyboard: [
-        [{ text: 'Открыть', web_app: { url: 'https://dadanetnet.vercel.app/' } }]
-      ],
-      resize_keyboard: true
-    }
-  });
-});
-
-bot.launch();
